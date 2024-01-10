7ed4dbaa4cad0fd02bcb230d0ea8ec87a13089eb
shabibokiribo <Niahtaylor806@yahoo.com>
2024-01-10 08:26:28 -0500

    commit

--- Assets/Scripts/CerberusScr.cs
+++ Assets/Scripts/CerberusScr.cs
@@ -2,16 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GabrielScr : MonoBehaviour
+public class CerberusScr : MonoBehaviour
 {
-    //Character Stats
-    public int hp = 100; //max 100
-    public int mp = 0; //max 50
-
-    public string charName = "Gabriel";
-
-    //MOVESET
-
     // Start is called before the first frame update
     void Start()
     {
