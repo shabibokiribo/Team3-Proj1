2fbb674e8f3fd8e7f64fb5702682c2fb06a90c88
Janokye <145355697+Janokye@users.noreply.github.com>
2024-01-18 09:55:01 -0500

    commit

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[System.Serializable]
+public class Dialogue {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public string name;
+   public string[] sentences;
 }
